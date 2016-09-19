04c3cafaedca03a0d9709694b11df154b52f0a50
Julio Arias <jarias@users.noreply.github.com>
2016-09-19 10:02:11 -0600

    Use ContentNegotiationResolver instead of the UserAgent
    ContentNegotiatingFieldValueResolver should use the ContentNegotiationResolver
    since this is the class that takes into consideration the produces
    property, that help resolve any case where the Accept header is not
    pressent.

--- extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/ContentNegotiatingFieldValueResolver.java
+++ extensions/servlet/src/main/java/com/stormpath/sdk/servlet/mvc/ContentNegotiatingFieldValueResolver.java
@@ -1,9 +1,16 @@
 package com.stormpath.sdk.servlet.mvc;
 
+import com.stormpath.sdk.servlet.filter.ContentNegotiationResolver;
+import com.stormpath.sdk.servlet.http.MediaType;
+import com.stormpath.sdk.servlet.http.UnresolvedMediaTypeException;
 import com.stormpath.sdk.servlet.http.UserAgent;
 import com.stormpath.sdk.servlet.http.UserAgents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,15 +18,21 @@ import java.util.Map;
  */
 public class ContentNegotiatingFieldValueResolver implements RequestFieldValueResolver {
 
+    private static final Logger log = LoggerFactory.getLogger(ContentNegotiatingFieldValueResolver.class);
+
     private final RequestFieldValueResolver JSON_BODY_RESOLVER = new JacksonFieldValueResolver();
     private final RequestFieldValueResolver REQ_PARAM_RESOLVER = new RequestParameterFieldValueResolver();
 
+    protected List<MediaType> produces;
+    private ContentNegotiationResolver contentNegotiationResolver = ContentNegotiationResolver.INSTANCE;
+
+    public void setProduces(List<MediaType> produces) {
+        this.produces = produces;
+    }
+
     @Override
     public String getValue(HttpServletRequest request, String fieldName) {
-
-        UserAgent ua = UserAgents.get(request);
-
-        if (ua.isJsonPreferred()) {
+        if (isJsonPreferred(request)) {
             return JSON_BODY_RESOLVER.getValue(request, fieldName);
         }
 
@@ -28,12 +41,19 @@ public class ContentNegotiatingFieldValueResolver implements RequestFieldValueRe
 
     @Override
     public Map<String, Object> getAllFields(HttpServletRequest request) {
-        UserAgent ua = UserAgents.get(request);
-
-        if (ua.isJsonPreferred()) {
+        if (isJsonPreferred(request)) {
             return JSON_BODY_RESOLVER.getAllFields(request);
         }
 
         return REQ_PARAM_RESOLVER.getAllFields(request);
+    }
+
+    protected boolean isJsonPreferred(HttpServletRequest request) {
+        try {
+            return MediaType.APPLICATION_JSON.equals(contentNegotiationResolver.getContentType(request, null, produces));
+        } catch (UnresolvedMediaTypeException e) {
+            log.debug("isJsonPreferred: Couldn't resolve content type: {}", e.getMessage());
+            return false;
+        }
     }
 }
