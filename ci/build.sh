e75320e0256c38c595b2ae6d483052c80ee37618
Julio Arias <jarias@users.noreply.github.com>
2016-09-21 14:55:23 -0600

    Remove tail of log

--- ci/build.sh
+++ ci/build.sh
@@ -13,7 +13,6 @@ if [ -n "$RUN_ITS" ]; then
   EXIT_CODE=$?
   if [ "$EXIT_CODE" -ne 0 ]; then
     error "Tests failed"
-    tail -n 100 $WORKDIR/target/tests.log
     ./ci/junit-errors-to-stdout.sh
     exit $EXIT_CODE
   fi
